45ef406a4449709b43a86b0aaf53f044029d8043
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 03:20:47 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
   def create
     recipe = Recipe.create(recipe_params)
     recipe.update(recipe_params)
-
+    redirect_to recipe
   end
 
   private
