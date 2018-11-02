331a87974c8f328765842835666a993b9de26f98
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 03:31:40 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,33 +1,33 @@
 class RecipesController < ApplicationController
   def show
-  @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:id])
+  end
 
-end
+  def index
+    @recipes = Recipe.all
+  end
 
-def index
-  @recipes = Recipe.all
-end
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+  end
 
-def new
-  @recipe = Recipe.new
-  @recipe.ingredients.build
-  @recipe.ingredients.build
-end
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
+  end
 
-def create
-  recipe = Recipe.create(recipe_params)
-  redirect_to recipe
-end
+  private
 
-def update
-  recipe = Recipe.find(params[:id])
-  recipe.update(recipe_params)
-  redirect_to recipe
-end
-
-private
-
-def recipe_params
-  params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
-end
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+      ingredients_attributes: [
+        :id,
+        :name,
+        :quantity
+      ]
+    )
+  end
 end
