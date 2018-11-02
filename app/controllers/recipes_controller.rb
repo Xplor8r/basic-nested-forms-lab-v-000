afcffecf2e7e358d0e6350e20cfa5247ea6b8323
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 03:19:21 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,14 +21,13 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(
-    :title,
-    ingredient_attributes:
-    [
-      :id,
-      :name,
-      :quantity
+      :title,
+      ingredient_attributes: [
+        :id,
+        :name,
+        :quantity
       ]
-      )
+    )
   end
 
 end
