e03f8ec700ce7c5784fd93b59bdc6a56046742f2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 03:23:08 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,8 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     if @recipe.ingredients.last.try(:name)
-  @recipe.ingredients.build
-end
+      @recipe.ingredients.build
+    end
   end
 
   def index
